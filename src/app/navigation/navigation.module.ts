e483b2e3bf6851db08c940324c1e31294e24fb4b
Marco <marcohavermans+github@hotmail.nl>
2018-01-08 13:19:52 +0100

    Seperated top/side navbar

---
+++ src/app/navigation/navigation.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavigationComponent } from './navigation.component';
+import { TopbarComponent } from './topbar/topbar.component';
+
+
+@NgModule({
+    declarations: [
+        SidebarComponent,
+        TopbarComponent,
+        NavigationComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [],
+    exports: [NavigationComponent]
+})
+export class NavigationModule { }
