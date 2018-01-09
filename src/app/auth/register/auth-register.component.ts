c87c043413dd4ea7a57055ae3bf62a31925b5cb1
Marco <marcohavermans+github@hotmail.nl>
2018-01-09 12:22:57 +0100

    Added making api call to register endpoint

--- src/app/auth/register/auth-register.component.ts
+++ src/app/auth/register/auth-register.component.ts
@@ -3,6 +3,7 @@ import { BaseComponent } from '../../shared/base/basecomponent.class';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RegisterModel } from './registermodel.class';
 import { equalValidator } from '../../shared/validator/equal.validator';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-auth-register',
@@ -19,11 +20,37 @@ export class AuthRegisterComponent extends BaseComponent {
     */
   public registerModel: RegisterModel;
 
-  constructor(private fb: FormBuilder) {
+  /**
+    * Determines if a submit is in progress
+    */
+  public submitInProgress: boolean;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
     super();
     this.registerModel = new RegisterModel();
 
     this.createForm();
+  }
+
+  public onSubmit(): void {
+    if (!this.registerForm.valid) {
+      return;
+    }
+
+    if (!this.registerModel.doPasswordsMatch()) {
+      return;
+    }
+
+    this.submitInProgress = true;
+
+    this.authService.register(this.registerModel.email, this.registerModel.password, this.registerModel.confirmPassword)
+      .subscribe((registered) => {
+        this.submitInProgress = false;
+
+      }, error => {
+        this.submitInProgress = false;
+        console.error(error);
+      });
   }
 
   /**
