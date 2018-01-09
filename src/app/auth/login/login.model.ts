ed7b171a55de109c60441b385bdc0298555d447a
Daniel van Bavel <dvanbavel@gmail.com>
2018-01-09 14:51:59 +0100

    commit

--- src/app/auth/login/login.model.ts
+++ src/app/auth/login/login.model.ts
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-export class Login {
-    
-    _id : string;
-    username: string;
-    password: string;
-    email: string;
-    token: string;
-=======
 export class User {
   username: string;
   password: string;
   email: string;
   token: string;
->>>>>>> develop
 
   constructor(username, password, email, token) {
     this.username = username;
