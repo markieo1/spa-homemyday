4029bc8ba0c5b38f4d81474b09b692cd69c834ff
Marco <marcohavermans+github@hotmail.nl>
2018-01-09 13:42:39 +0100

    Fixed linting issues

--- src/app/auth/login/login.model.ts
+++ src/app/auth/login/login.model.ts
@@ -1,16 +1,13 @@
 export class User {
-    
-    _id : string;
-    username: string;
-    password: string;
-    email: string;
-    token: string;
+  username: string;
+  password: string;
+  email: string;
+  token: string;
 
-    constructor(id, username, password, email, token) {
-        this._id = id;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.token = token;
-    }
+  constructor(username, password, email, token) {
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.token = token;
+  }
 }
