f9f06a9e06bc79b2b1a50cf35afeb7ab4cc33979
Danny Tam <kenwai2010@gmail.com>
2018-01-18 14:35:00 +0100

    fixed prod envirement url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://95.85.48.154/api/v1/',
-  homeMyDayUrl: 'http://localhost:49900/'
+  homeMyDayUrl: 'https://homemydaytest.azurewebsites.net/'
 };
