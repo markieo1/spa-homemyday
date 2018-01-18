544504bffb8efa415f746b177c83f76619686904
Hugo Zink <hugo.zink@hotmail.com>
2018-01-18 17:13:55 +0100

    Added form validator to edit user form.

--- src/app/user/user-edit/user-edit.component.ts
+++ src/app/user/user-edit/user-edit.component.ts
@@ -3,6 +3,8 @@ import { IUserToken } from '../../shared/interfaces/iusertoken.interface';
 import { AuthService } from '../../auth/auth.service';
 import { AlertService } from '../../alert/alert.service';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { equalValidator } from '../../shared/validator/equal.validator';
 
 @Component({
   selector: 'app-user-edit',
@@ -17,33 +19,47 @@ export class UserEditComponent implements OnInit {
   newPassword: string;
   newPasswordRepeat: string;
 
-  passwordsMatch = false;
+  detailsForm: FormGroup;
 
-  constructor(private authService: AuthService, private alertService: AlertService, private router: Router) { }
+  submitInProgress = false;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private alertService: AlertService, private router: Router) {
+    this.createForm();
+  }
 
   ngOnInit() {
     this.user = this.authService.getUserInfo();
   }
 
-  onPasswordChange() {
-    if (this.newPassword === this.newPasswordRepeat) {
-      this.passwordsMatch = true;
-    } else {
-      this.passwordsMatch = false;
+  /**
+   * Called when the form is submitted.
+   */
+  onSubmit() {
+    if (!this.detailsForm.valid) {
+      return;
     }
+
+    this.authService.changePassword(this.currentPassword, this.newPassword)
+    .subscribe(success => {
+      if (success) {
+        this.alertService.showSuccess('Details changed successfully!');
+        this.router.navigateByUrl('/user');
+      } else {
+        this.alertService.showError('An error has occurred while trying to change password.');
+      }
+    });
   }
 
-  save() {
-    if (this.passwordsMatch) {
-      this.authService.changePassword(this.currentPassword, this.newPassword)
-      .subscribe(success => {
-        if (success) {
-          this.alertService.showSuccess('Details changed successfully!');
-          this.router.navigateByUrl('/user');
-        } else {
-          this.alertService.showError('An error has occurred while trying to change password.');
-        }
-      });
-    }
+  /**
+   * Create the registration form.
+   */
+  private createForm() {
+    this.detailsForm = this.fb.group({
+      currentPassword: ['', Validators.required],
+      newPassword: ['', Validators.required],
+      newPasswordRepeat: ['', Validators.required]
+    }, {
+        validator: equalValidator('newPassword', 'newPasswordRepeat')
+    });
   }
 }
