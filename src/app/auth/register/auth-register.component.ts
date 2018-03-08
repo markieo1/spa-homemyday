0c42161ac477a7af11e2f13dc2eec831e3acd9db
Marco <marcohavermans+github@hotmail.nl>
2018-03-08 13:25:08 +0100

    Added validating of password by comparing to owasp requirements.

--- src/app/auth/register/auth-register.component.ts
+++ src/app/auth/register/auth-register.component.ts
@@ -6,6 +6,7 @@ import { equalValidator } from '../../shared/validator/equal.validator';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { AlertService } from '../../alert/alert.service';
+import * as owaspPasswordStrength from 'owasp-password-strength-test';
 
 @Component({
   selector: 'app-auth-register',
@@ -13,21 +14,26 @@ import { AlertService } from '../../alert/alert.service';
 })
 export class AuthRegisterComponent extends BaseComponent {
   /**
-    * The register form
-    */
+   * The register form
+   */
   public registerForm: FormGroup;
 
   /**
-    * The register model
-    */
+   * The register model
+   */
   public registerModel: RegisterModel;
 
   /**
-    * Determines if a submit is in progress
-    */
+   * Determines if a submit is in progress
+   */
   public submitInProgress: boolean;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router, private alertService: AlertService) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private alertService: AlertService
+  ) {
     super();
     this.registerModel = new RegisterModel();
 
@@ -43,30 +49,61 @@ export class AuthRegisterComponent extends BaseComponent {
       return;
     }
 
+    const testResult = owaspPasswordStrength.test(this.registerModel.password);
+
+    if (!testResult.strong) {
+      // Password not strong enough
+      this.alertService.showError(testResult.errors.join('\n'));
+      return;
+    }
+
     this.submitInProgress = true;
 
-    this.subscription = this.authService.register(this.registerModel.email, this.registerModel.password, this.registerModel.confirmPassword)
-      .subscribe((registered) => {
-        this.submitInProgress = false;
-        this.alertService.showSuccess('Successfully registered.');
-        this.router.navigate(['/login']);
-      }, error => {
-        console.error(error);
-        this.submitInProgress = false;
-        this.alertService.showError('An error has occurred while registering.');
-      });
+    this.subscription = this.authService
+      .register(
+        this.registerModel.email,
+        this.registerModel.password,
+        this.registerModel.confirmPassword
+      )
+      .subscribe(
+        registered => {
+          this.submitInProgress = false;
+          this.alertService.showSuccess('Successfully registered.');
+          this.router.navigate(['/login']);
+        },
+        error => {
+          console.error(error);
+          this.submitInProgress = false;
+
+          const jsonError = error.json();
+
+          if (jsonError.errors && jsonError.errors.length > 0) {
+            this.alertService.showError(jsonError.errors[0]);
+          } else {
+            this.alertService.showError(
+              'An error has occurred while registering.'
+            );
+          }
+        }
+      );
   }
 
   /**
-    * Creates the register form
-    */
+   * Creates the register form
+   */
   private createForm() {
-    this.registerForm = this.fb.group({
-      email: ['', Validators.compose([Validators.email, Validators.required])],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required]
-    }, {
+    this.registerForm = this.fb.group(
+      {
+        email: [
+          '',
+          Validators.compose([Validators.email, Validators.required])
+        ],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required]
+      },
+      {
         validator: equalValidator('password', 'confirmPassword')
-      });
+      }
+    );
   }
 }
